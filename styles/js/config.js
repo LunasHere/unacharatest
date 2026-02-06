9b593ad5c99743e2c2c3b7b1ba760cf922376363
Luna <90897209+LunasHere@users.noreply.github.com>
2026-02-06 10:50:47 -0600

    Update site title and URL in config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -15,8 +15,8 @@ let charadex = {};
 /* Any preview links will still show Charadex's information
 /* ==================================================================== */
 charadex.site = {
-  title: "Charadex",
-  url: "https://charadex-team.github.io/charadex-v1.0/",
+  title: "Una Chara",
+  url: "https://lunashere.github.io/unacharatest/",
   description: `A tool for organizing small ARPGs and species.`
 }
 
